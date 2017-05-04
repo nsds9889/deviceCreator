2a41adf2ee5634ab9a85fbad344383d788c99ce1
nsds9889 <patrick.staeuble9@gmail.com>
2017-05-04 11:02:54 -0400

    Create valid.php

---
+++ valid.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<title>WM Device Creator</title>
+	</head>
+<body>
+<center>
+CSV created successfully<p></p>
+
+Device will be generated with the following information<p></p>
+
+Device name: <?php echo $_POST["dname"]; ?><br>
+MAC address: <?php echo $_POST["mac"]; ?><br>
+Asset tag: <?php echo $_POST["asset"]; ?><br>
+Store type: <?php echo $_POST["store_type"]; ?>
+</center>
+
+</body>
+</html>
