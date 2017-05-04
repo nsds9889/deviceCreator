e1763ee1a0eae89f0543ce86ca86460e419a04e6
nsds9889 <patrick.staeuble9@gmail.com>
2017-05-04 11:13:24 -0400

    Update valid.php

--- valid.php
+++ valid.php
@@ -1,18 +1,21 @@
 <html>
 	<head>
+		<link rel="stylesheet" type="text/css" href="WMstyles.css">
 		<title>WM Device Creator</title>
+		
 	</head>
 <body>
-<center>
-CSV created successfully<p></p>
 
-Device will be generated with the following information<p></p>
+<div id="validate">
+Device will be generated with the following information - is this correct?
+<p>
 
-Device name: <?php echo $_POST["dname"]; ?><br>
+Device name: <?php echo $_POST["device"]; ?><br>
 MAC address: <?php echo $_POST["mac"]; ?><br>
 Asset tag: <?php echo $_POST["asset"]; ?><br>
 Store type: <?php echo $_POST["store_type"]; ?>
-</center>
+
+</div>
 
 </body>
 </html>
